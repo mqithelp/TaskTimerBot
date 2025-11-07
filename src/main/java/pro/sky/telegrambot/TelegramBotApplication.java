314daf46692104239cfbd0c38624b7e66e4c0d74
Max-Home <mqithelp@yandex.ru>
2025-11-08 00:55:06 +0300

    пункт 1.3 реализован

--- src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
+++ src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
@@ -11,5 +11,10 @@ public class TelegramBotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotApplication.class, args);
 	}
-
+/* так. Я сделал БД и пользователя.
+БД поднял локально в докере
+application.properties заполнил, но в добавил его в гитигнор
+Поэтому пишу камент чтобы запушить :-)
+На данном этапе проект запустился и в консоли видны сообщения боту
+ */
 }
