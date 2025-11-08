d523e1231cc95aeafb4ab2dca657b475653d8f34
Max-Home <mqithelp@yandex.ru>
2025-11-09 00:25:09 +0300

    Задача 2. Продолжение. Реализована часть 2.2 Создать сущность notification task

--- src/main/java/pro/sky/telegrambot/Entity/NotificationTask.java
+++ src/main/java/pro/sky/telegrambot/Entity/NotificationTask.java
@@ -1,4 +1,60 @@
 package pro.sky.telegrambot.Entity;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notification_task")
 public class NotificationTask {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "chat_id", nullable = false)
+    private Long chatId;
+
+    @Column(name = "message_text",nullable = false)
+    private String messageText;
+
+    @Column(name = "reminder_time",nullable = false)
+    private LocalDateTime reminderTime;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public LocalDateTime getReminderTime() {
+        return reminderTime;
+    }
+
+    public void setReminderTime(LocalDateTime reminderTime) {
+        this.reminderTime = reminderTime;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
