acb1255fb25f59ec081b992107ddf29ef4dc9bc7
Max-Home <mqithelp@yandex.ru>
2025-11-09 22:26:51 +0300

    Задача 2. Продолжение. Реализована часть 2.5 Научиться по шедулеру раз в минуту выбирать записи из БД, для которых должны быть отправлены нотификации

--- src/main/java/pro/sky/telegrambot/service/NotificationTaskService.java
+++ src/main/java/pro/sky/telegrambot/service/NotificationTaskService.java
@@ -1,9 +1,14 @@
 package pro.sky.telegrambot.service;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import pro.sky.telegrambot.Entity.NotificationTask;
 import pro.sky.telegrambot.dto.ParseReminderMessage;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.repository.NotificationTaskRepository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @Service
 public class NotificationTaskService {
@@ -24,4 +29,17 @@ public class NotificationTaskService {
             System.out.println("Ошибка: " + parsed.getErrorMessage());
         }
     }
+
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void runCheck() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime oneMinuteAgo = now.minusMinutes(1);
+        System.out.println("ReminderBySheduler started: " + LocalDateTime.now());
+        List<NotificationTask> tasks =  repository.findByReminderTimeBetween(oneMinuteAgo,now);
+        tasks.forEach(task -> {
+            System.out.println(task.getReminderTime() + ": " + task.getMessageText());
+        });
+
+    }
+
 }
